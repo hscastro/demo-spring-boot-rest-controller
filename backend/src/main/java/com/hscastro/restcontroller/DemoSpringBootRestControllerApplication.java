3af4425ae141baa7a4e94f8eea6906295d4c73ae
hscastro <halisonsc5@gmail.com>
2022-04-12 21:24:46 -0300

    first commit

---
+++ backend/src/main/java/com/hscastro/restcontroller/DemoSpringBootRestControllerApplication.java
@@ -0,0 +1,13 @@
+package com.hscastro.restcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootRestControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootRestControllerApplication.class, args);
+	}
+
+}
